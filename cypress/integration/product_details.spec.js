c3f6c5e73453354c49abeb349704d3575610c4f3
erikwehrmann <erik.wehrmann@gmail.com>
2023-01-31 19:15:14 +0000

    Product details page tested with Cypress

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,9 @@
+describe("HomePage", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("navigate to a product's page", () => {
+    cy.get(".products article").first().click();
+    cy.get(".product-detail").should("be.visible");
+  });
+});
